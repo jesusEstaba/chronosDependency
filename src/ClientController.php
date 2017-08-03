04534710c6c9802dab5b5fbc0ee162c8452a3419
Jesus Estaba <jeec.estaba@gmail.com>
2017-08-03 08:34:24 -0400

    clientCtrl

---
+++ src/ClientController.php
@@ -0,0 +1,95 @@
+<?php
+namespace ChronosDependency;
+
+use Illuminate\Http\Request;
+use Repo\Client;
+use Auth;
+
+trait ClientController
+ {
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index(Request $request)
+	{
+	    $search = $request->search;
+
+	    $clients = Client::where('companieId', Auth::user()->companieId)
+	        ->where(function ($query) use ($search) {
+	            if ($search) {
+	                $query->where('name', 'like', '%' . $search . '%');
+	            }
+	        })
+	        ->orderBy('id', 'desc')
+	        ->paginate(10);
+
+	    return view('client.index', compact('clients', 'search'));
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+	    return view('client.create');
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+	    Client::create([
+	        'name' => $request->name,
+	        'rif' => $request->rif ?? '',
+	        'address' => $request->address ?? '',
+	        'phone' => $request->phone ?? '',
+	        'companieId' => Auth::user()->companieId,
+	    ]);
+
+	    session()->flash('success', 'Cliente Creado.');
+	    
+	    return redirect('/clients');
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+	    //
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($id)
+	{
+	    //
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+	    //
+	}
+}
